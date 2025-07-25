ef65fb1eaa9fe2f683a97fd1aaf8c84e5bfc1046
Jed Lyle Fernandez <fernandezjedlyle@gmail.com>
2025-07-25 18:50:53 +0800

    run main via separate method

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/ProgressiveParanoia/go-game-server/internal/controller"
 	"github.com/ProgressiveParanoia/go-game-server/internal/handlers"
 	"github.com/ProgressiveParanoia/go-game-server/internal/repo/memory"
@@ -8,13 +11,22 @@ import (
 )
 
 func main() {
+	if err := run(); err != nil {
+		fmt.Errorf("error running application %v")
+		os.Exit(1)
+	}
+}
+
+func run() error {
 	// Initialize the router
 	r := gin.Default()
 
-	memRepo := memory.NewMemoryRepository()
+	userRepo := memory.NewUserRepository()
 
 	// Initialize the user controller
-	userController := controller.NewUserController(memRepo)
+	userController := controller.NewUserController(userRepo)
 
 	handlers.InitRouter(r, userController)
+
+	return nil
 }
