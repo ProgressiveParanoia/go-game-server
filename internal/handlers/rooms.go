09d39567a65c91a9bb8b292c32e4ccdc69839b13
Jed Lyle Fernandez <fernandezjedlyle@gmail.com>
2025-07-30 20:56:59 +0800

    - remove unused join endpoint - remove any pre-join methods, since we'll join rooms when we subscribe

--- internal/handlers/rooms.go
+++ internal/handlers/rooms.go
@@ -17,6 +17,10 @@ type (
 	}
 )
 
+var (
+	ErrRoomIDParamRequired = errors.New("room_id parameter is required")
+)
+
 func NewRoomHttpHandler(group *gin.RouterGroup, controller *controller.Room) *roomsHttpHandler {
 	return &roomsHttpHandler{
 		group:      group,
@@ -27,37 +31,44 @@ func NewRoomHttpHandler(group *gin.RouterGroup, controller *controller.Room) *ro
 func (h *roomsHttpHandler) RegisterRoutes() {
 	h.group.GET("/", h.GetRooms)
 	h.group.POST("/create", h.CreateRoom)
-	h.group.PUT("/join/:id", h.JoinRoom)
 	h.group.GET("/subscribe/:id", h.SubscribeToRoom)
 
 }
 
 func (h *roomsHttpHandler) CreateRoom(c *gin.Context) {
-	//TODO: add
-	//does room exist?
-}
+	roomId, err := h.controller.Create()
+	if err != nil {
+		respError(c, http.StatusInternalServerError, err.Error())
+	}
 
-func (h *roomsHttpHandler) JoinRoom(c *gin.Context) {
-
-	//TODO: add
-	//add myself to session, then create a socket?
-
-	//does the room exist?
-	//get associated user data from session
-	//append the user data to the ongoing room
-	//create socket for client to connect to
+	respSuccess(c, http.StatusCreated, roomId)
 }
 
 func (h *roomsHttpHandler) SubscribeToRoom(c *gin.Context) {
-	roomId := c.Param("room_id")
-	userId := c.Param("user_id")
+	roomId := c.Param("id")
+	//todo: get userID from context / session
+	userId := "add me"
+
+	if roomId == "" {
+		respError(c, http.StatusBadRequest, ErrRoomIDParamRequired.Error())
+		fmt.Println("Err with nil roomid")
+		return
+	}
+
 	err := h.controller.SubscribeToRoom(roomId, userId, c.Writer, c.Request)
 	if err != nil {
 		if errors.Is(err, context.Canceled) {
 			fmt.Printf("\n user id %v has disconnected from room id %v\n", userId, roomId)
+			//TODO: damn so deep. Clean this up?
+			err = h.controller.CleanUpEmptyRoomAfterDisconnect(roomId)
+			if err != nil {
+				respError(c, http.StatusInternalServerError, err.Error())
+			}
+
 			return
 		}
 
+		fmt.Println("Err with subscription ", err.Error())
 		respError(c, http.StatusInternalServerError, err.Error())
 	}
 }
